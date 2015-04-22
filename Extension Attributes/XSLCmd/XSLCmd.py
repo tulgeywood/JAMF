5866c1f467d10ee550fefe6bfead7df3f2717c9c
tulgeywood <jesses@s926-3.local>
2015-04-22 17:07:13 -0400

    Detect attribute for new malware

---
+++ Extension Attributes/XSLCmd/XSLCmd.py
@@ -0,0 +1,23 @@
+import os
+
+files = ["/Users/$user/Library/LaunchAgents/clipboardd",
+         "/Users/$user/Library/Logs/clipboardd",
+         "/Users/$user/Library/LaunchAgents/com.apple.service.clipboardd.plist",
+         "/Users/$user/.fontset/pxupdate.ini",
+         "/Users/$user/.fontset/chkdiska.dat",
+         "/Users/$user/.fontset/chkdiskc.dat",
+         "/Users/$user/Library/Logs/BackupData/<year><month><day>_<hr>_<min>_<sec>_keys.log"]
+
+files_found = ""
+
+for file in files:
+    if os.path.isfile(file):
+        if len(files_found) == 0:
+            files_found += file
+        else:
+            files_found.append(", " + file)
+
+if len(files_found) > 0:
+    print "<result>" + files_found + "</result>"
+else:
+    print "<result>Clean</result>"
