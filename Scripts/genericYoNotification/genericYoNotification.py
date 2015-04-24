5a29189a6eaf823b74dc02546a484b5265612536
tulgeywood <jesses@s926-4.local>
2015-04-24 14:59:19 -0400

    fixed string processing error

--- Scripts/genericYoNotification/genericYoNotification.py
+++ Scripts/genericYoNotification/genericYoNotification.py
@@ -19,21 +19,21 @@ import sys
 ##########################################################################################
 
 YO = "/Applications/Utilities/Yo.app/Contents/MacOS/yo"
-title = ['-t',str(sys.argv[4])]
-subtitle = ['-s',str(sys.argv[5])]
-info = ['-n',str(sys.argv[6])]
-button1 = ['-b',str(sys.argv[7])]
-button2 = ['-o',str(sys.argv[8])]
-icon = ['-i',str(sys.argv[9])]
-image = ['-c',str(sys.argv[10])]
-action = ['-a',str(sys.argv[11])]
+title = ['-t',('\"' + str(sys.argv[4]) + '\"')]
+subtitle = ['-s',('\"' + str(sys.argv[5]) + '\"')]
+info = ['-n',('\"' + str(sys.argv[6]) + '\"')]
+button1 = ['-b',('\"' + str(sys.argv[7]) + '\"')]
+button2 = ['-o',('\"' + str(sys.argv[8]) + '\"')]
+icon = ['-i',('\"' + str(sys.argv[9]) + '\"')]
+image = ['-c',('\"' + str(sys.argv[10]) + '\"')]
+action = ['-a',('\"' + str(sys.argv[11]) + '\"')]
 
 flags = [title,subtitle,info,button1,button2,icon,image,action]
 
 command_string = ''
 
 for flag in flags:
-    if len(flag[1]) > 0:
+    if flag[1] != '\"\"':
         command_string += flag[0] + ' ' + flag[1] + ' '
 
 print command_string
